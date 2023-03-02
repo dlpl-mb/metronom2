642b474710dd5da2228031f112864a01843d7aa0
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2023-03-02 21:55:15 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    basic.pause(1000)
+    for (let index = 0; index < 4; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(1000)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    basic.pause(500)
+    for (let index = 0; index < 4; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
 })
 basic.showLeds(`
     . # # # .
